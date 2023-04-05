3a8c80ab373fef180115161153dc362b324715de
ajitesh <ajitesh314mishra@gmail.com>
2023-04-05 21:04:20 +0530

    bagute

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.cdnfonts.com/css/impact" rel="stylesheet" />
+        <link href="https://fonts.cdnfonts.com/s/87898/unicode.impact.woff" rel="stylesheet" />
         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       </Head>
