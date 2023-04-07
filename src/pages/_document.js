f93d636b85a56ef240fde85e1877c74fb9e46565
ajitesh <ajitesh314mishra@gmail.com>
2023-04-07 17:45:37 +0530

    dd

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.cdnfonts.com/s/87898/unicode.impact.woff" rel="stylesheet" />
+      <link href="https://www.dafontfree.net/embed/aW1wYWN0LXJlZ3VsYXImZGF0YS8yNS9pLzEyOTYxMi9pbXBhY3QudHRm" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
